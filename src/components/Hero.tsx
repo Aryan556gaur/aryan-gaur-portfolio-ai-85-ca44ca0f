39012e3963b1346505eb5538782e74792b5f3f8e
Aryan Gaur <aryangaur556@gmail.com>
2026-06-09 00:07:38 +0530

    Update link to new Google Drive file

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -166,7 +166,7 @@ const Hero = () => {
             <ArrowRight size={18} />
           </a>
           <a 
-            href="https://drive.google.com/file/d/1kaD3tetfRI3eWXRetZhG0ZeEGZP1FbeZ/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Sw5PJJlnHRw1TYIlKn4ycmjtDg4_G9Yb/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="btn-secondary"
